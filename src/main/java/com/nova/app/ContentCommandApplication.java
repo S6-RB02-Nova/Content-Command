23ba824ef63b8b76261e92fd394f9472d1e0e3b6
Tony <51972581+tony1658@users.noreply.github.com>
2022-06-19 16:10:15 +0200

    Initial config

---
+++ src/main/java/com/nova/app/ContentCommandApplication.java
@@ -0,0 +1,13 @@
+package com.nova.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCommandApplication.class, args);
+	}
+
+}
